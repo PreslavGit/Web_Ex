a07adaa1e296acaeefda3fc7d72d7bf3c86be234
RadiPopova <raditto22@gmail.com>
2023-12-06 22:09:59 +0200

    note

--- Data/DB_Context.cs
+++ Data/DB_Context.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using webex.Models;
 
 public class Db_Context : IdentityDbContext<IdentityUser>
 {
@@ -9,5 +10,13 @@ public class Db_Context : IdentityDbContext<IdentityUser>
     {
     }
 
-    // public DbSet<Model> Model { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<MuscleGroup>().Property(e => e.Location).HasConversion<string>();
+        builder.Entity<MuscleGroup>().Property(e => e.MuscleFunction).HasConversion<string>();
+    }
+
+    public DbSet<MuscleGroup> MuscleGroups { get; set; }
 }
