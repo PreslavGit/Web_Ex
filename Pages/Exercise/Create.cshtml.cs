07f8a21bcecebf2a87e6194989e8a885ac4b9474
Preslav <preslavk1201@gmail.com>
2023-12-12 17:22:13 +0200

    partially done exercise create page

--- Pages/Exercise/Create.cshtml.cs
+++ Pages/Exercise/Create.cshtml.cs
@@ -5,36 +5,42 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using webex.Data;
+using webex.Models;
 
 namespace webex.Pages_Exercise
 {
     public class CreateModel : PageModel
     {
         private readonly webex.Data.DbContextEx _context;
+        public List<MuscleGroup> MuscleGroups { get; set; }
+        [BindProperty]
+        public List<int> SelectedMuscleGroupIds { get; set; }
+        [BindProperty]
+        public Exercise Exercise { get; set; } = default!;
 
         public CreateModel(webex.Data.DbContextEx context)
         {
             _context = context;
         }
 
-        public IActionResult OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
+            MuscleGroups = await _context.MuscleGroups.ToListAsync();
             return Page();
         }
 
-        [BindProperty]
-        public Exercise Exercise { get; set; } = default!;
-        
-
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid || _context.Exercises == null || Exercise == null)
+            if (!ModelState.IsValid || _context.Exercises == null || Exercise == null)
             {
-                return Page();
+                return await OnGetAsync();
             }
-
+            foreach (var id in SelectedMuscleGroupIds){
+                System.Console.WriteLine(id);
+            }
+            return await OnGetAsync();
             _context.Exercises.Add(Exercise);
             await _context.SaveChangesAsync();
 
