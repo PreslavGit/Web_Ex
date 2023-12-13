2ce4e00192075e73a817ae2efc384f367c246562
RadiPopova <raditto22@gmail.com>
2023-12-13 19:45:48 +0200

    Fixed Exercises Pages and Login/Register

--- Pages/Exercise/Edit.cshtml.cs
+++ Pages/Exercise/Edit.cshtml.cs
@@ -6,14 +6,20 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using webex.Data;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.VisualBasic;
+using webex.Models;
+
 
 namespace webex.Pages_Exercise
 {
     public class EditModel : PageModel
     {
         private readonly webex.Data.DbContextEx _context;
-
+        public List<MuscleGroup> MuscleGroups { get; set; }
+        public List<int> SelectedMuscleGroups = new List<int>();
+        [BindProperty]
+        public List<int> SelectedMuscleGroupIds { get; set; }
         public EditModel(webex.Data.DbContextEx context)
         {
             _context = context;
@@ -29,12 +35,16 @@ namespace webex.Pages_Exercise
                 return NotFound();
             }
 
-            var exercise =  await _context.Exercises.FirstOrDefaultAsync(m => m.Id == id);
+            var exercise = await _context.Exercises
+                .Include(e => e.MuscleGroups)
+                .FirstOrDefaultAsync(m => m.Id == id);
             if (exercise == null)
             {
                 return NotFound();
             }
             Exercise = exercise;
+            MuscleGroups = await _context.MuscleGroups.ToListAsync();
+            this.MapChecked();
             return Page();
         }
 
@@ -51,6 +61,10 @@ namespace webex.Pages_Exercise
 
             try
             {
+                // _context.Update(Exercise);
+
+                MuscleGroups = await _context.MuscleGroups.ToListAsync();
+                this.AppendMuscleGroups();
                 await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
@@ -65,12 +79,55 @@ namespace webex.Pages_Exercise
                 }
             }
 
+
             return RedirectToPage("./Index");
         }
 
         private bool ExerciseExists(int id)
         {
-          return (_context.Exercises?.Any(e => e.Id == id)).GetValueOrDefault();
+            return (_context.Exercises?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        private void MapChecked()
+        {
+
+            foreach (var emg in Exercise.MuscleGroups)
+            {
+                foreach (var mg in MuscleGroups)
+                {
+                    if (emg.Id == mg.Id)
+                    {
+                        SelectedMuscleGroups.Add(mg.Id);
+                    }
+                }
+            }
+        }
+
+        private void AppendMuscleGroups()
+        {
+            this.deleteOldMuscleGroups();
+
+            foreach (var muscleGroupId in SelectedMuscleGroupIds)
+            {
+                var muscleGroupEntity = MuscleGroups.Find(mg => mg.Id == muscleGroupId);
+                if (muscleGroupEntity != null)
+                {
+                    Exercise.MuscleGroups.Add(muscleGroupEntity);
+                }
+            }
+        }
+
+        private void deleteOldMuscleGroups(){
+            var muscleGroupsToDelete = _context.MuscleGroups
+                .Where(mg => mg.Exercises.Any(e => e.Id == Exercise.Id))
+                .Include(mg => mg.Exercises)
+                .ToList();
+
+            foreach (var muscleGroup in muscleGroupsToDelete)
+            {   
+                var removedCnt = muscleGroup.Exercises.RemoveAll(e => e.Id == Exercise.Id);
+            }
+            _context.SaveChanges();
         }
     }
 }
