2b6142ed61f735f2e981f0b360df547d973f2639
RadiPopova <raditto22@gmail.com>
2023-12-15 10:17:08 +0200

    Add Workout Scaffold

---
+++ Pages/Workouts/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using webex.Data;
+
+namespace webex.Pages.Workouts
+{
+    public class IndexModel : PageModel
+    {
+        private readonly webex.Data.DbContextEx _context;
+
+        public IndexModel(webex.Data.DbContextEx context)
+        {
+            _context = context;
+        }
+
+        public IList<Workout> Workout { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Workouts != null)
+            {
+                Workout = await _context.Workouts.ToListAsync();
+            }
+        }
+    }
+}
