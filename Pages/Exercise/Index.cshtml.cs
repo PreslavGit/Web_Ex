f275a7ff6f3c83a4184f5dc4b7f2e52c9c847abb
RadiPopova <raditto22@gmail.com>
2024-01-09 10:05:29 +0200

    sort,filter and page for Ex, Wo and MG pages

--- Pages/Exercise/Index.cshtml.cs
+++ Pages/Exercise/Index.cshtml.cs
@@ -6,10 +6,33 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using webex.Data;
+using webex.Migrations;
 using webex.Models;
 
 namespace webex.Pages_Exercise
 {
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            this.AddRange(items);
+        }
+        public bool HasPreviousPage { get { return (PageIndex > 1); } }
+        public bool HasNextPage { get { return (PageIndex < TotalPages); } }
+        public static async Task<PaginatedList<T>> CreateAsync(
+        IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip(
+            (pageIndex - 1) * pageSize)
+            .Take(pageSize).ToListAsync();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
     public class IndexModel : PageModel
     {
         private readonly webex.Data.DbContextEx _context;
@@ -19,15 +42,55 @@ namespace webex.Pages_Exercise
             _context = context;
         }
 
-        public IList<Exercise> Exercise { get;set; } = default!;
+        public string NameSort { get; set; }
+        public string CurrentSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public PaginatedList<Exercise> Exercise { get; set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
+            CurrentFilter = searchString;
+
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "Name" : "";
+
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            CurrentSort = sortOrder;
+            CurrentFilter = searchString;
+
+            IQueryable<Exercise> exercisesIQ = from ex in _context.Exercises
+                                               select ex;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                exercisesIQ = exercisesIQ.Where(ex =>
+                ex.Name.Contains(searchString)
+               || ex.MuscleGroups.Any(m => m.Name.Contains(searchString))
+                );
+            }
+
+            switch (sortOrder)
+            {
+                case "Name_desc":
+                    exercisesIQ = exercisesIQ.OrderByDescending(s => s.Name);
+                    break;
+                case "Name":
+                    exercisesIQ = exercisesIQ.OrderBy(s => s.Name);
+                    break;
+                
+            }
             if (_context.Exercises != null)
             {
-                Exercise = await _context.Exercises 
-                    .Include(ex => ex.MuscleGroups)
-                    .ToListAsync();
+                int pageSize = 5;
+                Exercise = await PaginatedList<Exercise>.CreateAsync(
+                    exercisesIQ.Include(ex => ex.MuscleGroups).AsNoTracking(), pageIndex ?? 1, pageSize);
             }
         }
     }
--- Pages/Workouts/Index.cshtml.cs
+++ Pages/Workouts/Index.cshtml.cs
@@ -10,6 +10,28 @@ using webex.Models;
 
 namespace webex.Pages.Workouts
 {
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            this.AddRange(items);
+        }
+        public bool HasPreviousPage { get { return (PageIndex > 1); } }
+        public bool HasNextPage { get { return (PageIndex < TotalPages); } }
+        public static async Task<PaginatedList<T>> CreateAsync(
+        IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip(
+            (pageIndex - 1) * pageSize)
+            .Take(pageSize).ToListAsync();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
     public class IndexModel : PageModel
     {
         private readonly webex.Data.DbContextEx _context;
@@ -18,18 +40,60 @@ namespace webex.Pages.Workouts
         {
             _context = context;
         }
+        public string NameSort { get; set; }
+        public string CurrentSort { get; set; }
+        public string CurrentFilter { get; set; }
         [BindProperty]
-        public IList<Models.Workout> Workout { get; set; } = default!;
+        public PaginatedList<Models.Workout> Workout { get; set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
+            CurrentFilter = searchString;
+
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "Name" : "";
+
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            CurrentSort = sortOrder;
+            CurrentFilter = searchString;
+
+            IQueryable<Workout> workoutsIQ = from wo in _context.Workouts
+                                                    select wo;
+            
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                workoutsIQ = workoutsIQ.Where(ex =>
+                ex.Name.Contains(searchString)
+               || ex.WorkoutExercises.Any(m => m.Exercise.Name.Contains(searchString))
+                );
+            }
+            switch (sortOrder)
+            {
+                case "Name_desc":
+                    workoutsIQ = workoutsIQ.OrderByDescending(s => s.Name);
+                    break;
+                case "Name":
+                    workoutsIQ = workoutsIQ.OrderBy(s => s.Name);
+                    break;
+                
+            }
+
             if (_context.Workouts != null)
             {
-                Workout = await _context.Workouts
-                .Include(w => w.WorkoutExercises)
+                int pageSize = 5;
+                Workout = await PaginatedList<Workout>.CreateAsync(
+                workoutsIQ.Include(w => w.WorkoutExercises)
                     .ThenInclude(ex => ex.Exercise)
-                .ToListAsync();
+                .AsNoTracking(), pageIndex ?? 1, pageSize);
             }
         }
+
     }
 }
